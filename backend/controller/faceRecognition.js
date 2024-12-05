38a4ef83b59d9f817f7ecd8246555dad56ee772f
karthik092003 <karthik.s092003@gmail.com>
2024-12-05 14:44:06 +0530

    added getUserFaceDescripton func

--- backend/controller/faceRecognition.js
+++ backend/controller/faceRecognition.js
@@ -20,4 +20,20 @@ const storeFaceData = async (req, res) => {
   }
 };
 
-module.exports = { storeFaceData };
+const getUserFaceDescripton = async (req,res) => {
+  const {userId} = req.query;
+  if(!userId){
+    return res.status(400).json({ msg: "userId is needed" });
+  }
+  try {
+    const data = await Face.find({userId:userId});
+    if(data.length === 0){
+      return res.status(404).json({ msg: "userId not found"})
+    }
+    return res.status(200).json({ msg: "Data found successfully!", data: data[0] })
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+module.exports = { storeFaceData,getUserFaceDescripton };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {storeFaceData} = require("../controller/faceRecognition")
+const {storeFaceData,getUserFaceDescripton} = require("../controller/faceRecognition")
 
 
 router.route("/storeData").post(storeFaceData)
+router.route("/getData").get(getUserFaceDescripton)
 
 module.exports = router;
